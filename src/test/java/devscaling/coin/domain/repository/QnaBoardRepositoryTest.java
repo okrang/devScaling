a2c84c7125003a2adaf5d2985702dca6bd5af8ef
hwangjeyeon <kingpele9018@gmail.com>
2024-09-26 00:56:58 +0900

    Test: 익명유저/QnA게시판/QnA댓글 Repository 기능 테스트

---
+++ src/test/java/devscaling/coin/domain/repository/QnaBoardRepositoryTest.java
@@ -0,0 +1,68 @@
+package devscaling.coin.domain.repository;
+
+import devscaling.coin.domain.entity.Anonymous;
+import devscaling.coin.domain.entity.QnaBoard;
+import devscaling.coin.domain.entity.QnaComment;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@DataJpaTest
+class QnaBoardRepositoryTest {
+
+    @Autowired
+    QnaBoardRepository qnaBoardRepository;
+    @Autowired
+    AnonymousRepository anonymousRepository;
+
+    Anonymous anonymous;
+    QnaBoard qnaBoard;
+    QnaComment qnaComment;
+
+    @BeforeEach
+    void before(){
+        qnaBoard = QnaBoard.builder()
+                .title("시스템 오류")
+                .content("장애 확인 바람")
+                .build();
+        anonymous = Anonymous.builder()
+                .password("1234")
+                .build();
+    }
+
+    @Test
+    @DisplayName("QnA게시판에 잘 저장되는지 테스트")
+    void saveQnaBoardTest(){
+        Long id = qnaBoardRepository.save(qnaBoard).getId();
+
+        assertThat(qnaBoard.getContent())
+                .isEqualTo(qnaBoardRepository
+                        .findById(id)
+                        .get()
+                        .getContent());
+        assertThat(qnaBoard.getTitle())
+                .isEqualTo(qnaBoardRepository
+                        .findById(id)
+                        .get()
+                        .getTitle());
+    }
+
+    @Test
+    @DisplayName("익명유저 - QnA게시판간 연관관계 테스트")
+    void correlationTest(){
+        Long id = anonymousRepository.save(anonymous).getId();
+        qnaBoard.setAnonymousQnaBoard(anonymous);
+        qnaBoardRepository.save(qnaBoard);
+
+
+        assertThat(qnaBoard.getAnonymousMember().getPassword())
+                .isEqualTo(anonymousRepository.findById(id)
+                        .get().getPassword());
+    }
+
+}
