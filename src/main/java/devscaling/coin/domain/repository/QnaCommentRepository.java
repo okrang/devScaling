aa0650e03c9cf0f4be596e9e7106f178d575a0cf
hwangjeyeon <kingpele9018@gmail.com>
2024-09-26 00:56:46 +0900

    Feat: 익명유저/QnA게시판/QnA댓글 Repository 개발

---
+++ src/main/java/devscaling/coin/domain/repository/QnaCommentRepository.java
@@ -0,0 +1,8 @@
+package devscaling.coin.domain.repository;
+
+import devscaling.coin.domain.entity.QnaComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QnaCommentRepository extends JpaRepository<QnaComment, Long> {
+
+}
